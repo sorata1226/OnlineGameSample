ba3fa2077b522e7fe9901043acd528932d5b0aa5
sorata1226 <43601586+sorata1226@users.noreply.github.com>
2018-11-25 01:04:38 +0900

    Add exit files

---
+++ OnlineGame/Assets/Scripts/PhotonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonController : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(
+            player.name,
+            new Vector3(0f, 1f, 0f),
+            Quaternion.identity,
+            0
+         );
+
+        GameObject mainCamera = GameObject.FindWithTag("MainCamera");
+        mainCamera.GetComponent<ThirdPersonCamera>().enabled = true;
+    }
+}
